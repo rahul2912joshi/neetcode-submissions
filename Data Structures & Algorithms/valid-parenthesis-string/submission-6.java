a00c5edd4f6c19cced621bb117ce8348f5924844
rahul2912joshi <rahul2912joshi@gmail.com>
2026-05-04 23:59:44 +0530

    Add: valid-parenthesis-string - submission-6

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-6.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int openMin = 0, openMax = 0;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                openMin++;
+                openMax++;
+            } else if(ch==')'){
+                openMin--;
+                openMax--;
+            } else{
+                openMin--;
+                openMax++;
+            }
+            if(openMax<0) return false;
+            if(openMin<0) openMin=0;
+        }
+        return openMin<=0;
+    }
+}
+
+// ((((()))***
+// open_min -2
+// open_max 18
