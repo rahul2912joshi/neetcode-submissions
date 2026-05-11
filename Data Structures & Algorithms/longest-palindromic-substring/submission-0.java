6990628e06cedad306d89c40dcc37bb0496a4c95
rahul2912joshi <rahul2912joshi@gmail.com>
2026-05-12 00:46:01 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length() <= 1) return s;
+        int[] values = new int[3];
+        int[] result = new int[3];
+
+        for(int i=0; i<s.length(); i++){
+            findPalindrome(s, i,i, values);
+            if(values[0] > result[0]){
+                result[0] = values[0];
+                result[1] = values[1];
+                result[2] = values[2];
+            }
+            findPalindrome(s,i,i+1,values);
+            if(values[0] > result[0]){
+                result[0] = values[0];
+                result[1] = values[1];
+                result[2] = values[2];
+            }
+        }
+        return s.substring(result[1],result[2]+1);
+    }
+}
+
+public void findPalindrome(String s, int mid1, int mid2, int[] values){
+    while(mid1>0 && mid2<s.length()-1 && s.charAt(mid1)==s.charAt(mid2)){
+        mid1--;
+        mid2++;
+    }
+    if(mid1>=0 && mid2<s.length() && s.charAt(mid1)==s.charAt(mid2)){
+        values[1] = mid1;
+        values[2] = mid2;
+    } else{
+        values[1] = mid1+1;
+        values[2] = mid2-1;
+    }
+    values[0] = values[2]-values[1]+1;
+}
+
+// ababd len = 5
+// mid1 = 0
+// mid2 = 2
+// values = 0,0,0
